076e4a94ebae086d52f02111219adb6fdb216cd8
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-24 16:29:45 +0300

    configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
+        clean: true,
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
     },
     module: {
         rules: [
