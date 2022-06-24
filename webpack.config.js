fd2e721a35e63c6942a8ef08f8ed5ad983ca6f2f
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-24 13:20:39 +0300

    configure styling loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,19 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ]
+    }
 }
