9ea4166a994eba3452db08e2e405d063b3b6aa81
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-24 19:42:14 +0300

    link image src

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import usersList from "./users";
 import './styles/main.scss';
-import mine from './assets/mine.jpeg';
+import mine from './assets/mine.png';
+
+const profileImg = document.getElementById('img-profile');
+profileImg.src = mine;
 
 console.log(usersList());
