1f766481602dca203a42e0d1823d5043d14089eb
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-06-25 08:59:32 +0300

    implements fetch userprofile

--- src/users.js
+++ src/users.js
@@ -1,5 +1,20 @@
+import axios from 'axios';
+
 const usersList = () => {
-    return "user's list api call";
+    const config = {
+        headers: {
+            Accept: 'application/json'
+        }
+    }
+    axios.get('https://jsonplaceholder.typicode.com/users', config)
+        .then(res => {
+            let index = Math.floor(Math.random()*res.data.length);
+            document.getElementById('profile-name').innerHTML = res.data[index].name;
+            document.getElementById('profile-email').innerHTML = res.data[index].email;
+            document.getElementById('profile-website').innerHTML = res.data[index].website;
+            document.getElementById('profile-phone').innerHTML = res.data[index].phone;
+            document.getElementById('profile-company').innerHTML = res.data[index].company.name;
+        })
 }
 
 export default usersList;
